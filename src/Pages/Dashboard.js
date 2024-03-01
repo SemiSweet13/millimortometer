5df6146cb3f5625bd7d6dce094f580a983df94a5
Jack Malone <jack.malone.2021@mumail.ie>
2024-03-01 15:21:00 +0000

    Start of validation, needs to be implemented still

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -6,14 +6,13 @@ import { styled } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimeField } from '@mui/x-date-pickers/TimeField';
-import SelectorMenu from "../Components/SelectorMenu"; 
+import Button from '@mui/material/Button';
+import SelectorMenu from "../Components/SelectorMenu";
 import MapComponent from "../Components/MapComponent";
 import DrawerComponent from "../Components/DrawerComponent"
 
 
-
 const dayOptions = [
-  { value: '', label: 'None' },
   { value: 'Monday', label: 'Monday' },
   { value: 'Tuesday', label: 'Tuesday' },
   { value: 'Wednesday', label: 'Wednesday' },
@@ -24,12 +23,11 @@ const dayOptions = [
 
 ]
 const genderOptions = [
-  { value: '', label: 'None' },
   { value: 'Male', label: 'Male' },
-  { value: 'Female', label: 'Female' }
+  { value: 'Female', label: 'Female' },
+  { value: 'Prefer Not to Say', label: 'Prefer Not to Say' }
 ]
 const vehicleOptions = [
-  { value: '', label: 'None' },
   { value: 'Car', label: 'Car' },
   { value: 'MotorBike', label: 'MotorBike' }
 ]
@@ -43,22 +41,49 @@ const StyledTypography = styled(Typography)(({ theme }) => ({
 //component called in index.js
 export default function Dashboard() {
   //useStates needed for the dashboard
-  const [open, setOpen] = React.useState(false)
   const [day, setDay] = React.useState('')
   const [gender, setGender] = React.useState('')
-  const [vehicle, setVehicle] = React.useState('')  
+  const [vehicle, setVehicle] = React.useState('')
+
+  const [hasAge, setHasAge] = useState(false);
+  const [hasGender, setHasGender] = useState(false);
+  const [hasTime, setHasTime] = useState(false);
+  const [hasVehicle, setHasVehicle] = useState(false);
+  const [hasDay, setHasDay] = useState(false);
+
+  //Using a UseState Object to check all forms for validation
   //handle change functions
   const handleDayChange = (event) => {
     setDay(event.target.value)
+    //if there is a chosen day update validator state
+    if (day) {
+      setHasDay(true)
+    }
   }
   const handleGenderChange = (event) => {
     setGender(event.target.value)
+    if (gender) {
+      setHasGender(true)
+    }
   }
   const handleVehicleChange = (event) => {
     setVehicle(event.target.value)
+    if (vehicle) {
+      setHasVehicle(true)
+    }
+  }
+  //Section to VALIDATE all Input Forms  
+  const handleAgeValidation = (val) => {
+    //only accepts age range of 17-120 inclusive
+    if (val >= 17 || val <= 120) {
+      //set hasAge to true
+      setHasAge(true)
+    }
+    else {
+      setHasAge(false)
+    }
   }
 
-  
   //elements to be rendered
   return (
     <div>
@@ -77,11 +102,11 @@ export default function Dashboard() {
             </StyledTypography>
           </Toolbar>
         </AppBar>
-        {/*persistent left drawer component */}    
+        {/*persistent left drawer component */}
         <DrawerComponent></DrawerComponent>
 
         {/*MAIN component, i.e. dashboard */}
-        <Box component="main" sx={{ flexGrow: 1, p: 0.5, border: 1, pb:60 }}>
+        <Box component="main" sx={{ flexGrow: 1, p: 0.5, border: 1, pb: 60 }}>
           <Toolbar />
           <Grid container spacing={3} justifyContent="center" sx={{ p: '1rem' }} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
 
@@ -89,7 +114,12 @@ export default function Dashboard() {
             <Grid item xs={6}>
               <Box display="flex" justifyContent="center">
                 <TextField fullWidth
-                  label='Driver Age' type="number"></TextField>
+                  label='Driver Age' type="number"
+                  inputProps={{
+                    min: 17,
+                    max: 120
+                  }}>
+                </TextField>
               </Box>
             </Grid>
             <Grid item xs={6} >
@@ -110,14 +140,18 @@ export default function Dashboard() {
             <Grid item xs={12}>
               <SelectorMenu label='Vehicle Type' options={vehicleOptions} vehicle={vehicle} handleChange={handleVehicleChange}></SelectorMenu>
             </Grid>
-          </Grid>      
-          <div style={{paddingLeft:15}}> 
+          </Grid>
+          <div style={{ paddingLeft: 15 }}>
             <MapComponent>
             </MapComponent>
-            </div>
-              
+          </div>
+          <Box component="main" sx={{ flexGrow: 1, p: 2}}>
+            <Button size="large" variant="outlined" fullWidth>Calculate Risk</Button>
+          </Box>
+
+
         </Box>
-        
+
         <FooterComponent></FooterComponent>
       </Box>
     </div>//end of container div
